cd4474fb357c1b52c678345a00817ab2605d2fde
Dada Joshua <joshwebprog@gmail.com>
2024-04-25 12:37:54 +0100

    Updated with Animation

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 // Nav Links
 
-const allNavLinks = document.querySelectorAll(".header ul li:not(:first-of-type)")
+const allNavLinks = document.querySelectorAll(".nav-link:not(:first-of-type)")
 
 // Containers
 const home = document.querySelector(".hero-section");
@@ -22,46 +22,52 @@ function isAtTheTop(element){
     const theContainer = element.getBoundingClientRect();
     // console.log(theContainer.top);
     return(
-        theContainer.top >= -(window.innerHeight / 2) && theContainer.left>=0 && theContainer.right<=(window.innerWidth || documentElement.clientWidth)
+        // theContainer.top >= -(window.innerHeight * (2 / 3)) && theContainer.left>=0 && theContainer.right<=(window.innerWidth || documentElement.clientWidth)
+        theContainer.top >= -(window.innerHeight * (2 / 3))
     );
 }
 
-setInterval(() => {
-    if (isAtTheTop(home)) {
-        allNavLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
-    }else if (isAtTheTop(workContainer)) {
-        allNavLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
-        allNavLinks[0].classList.add("active-link");
-    }else if (isAtTheTop(servicesContainer)) {
-        allNavLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
-        allNavLinks[1].classList.add("active-link");
-    }else if (isAtTheTop(pricesContainer)) {
-        allNavLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
-        allNavLinks[2].classList.add("active-link");
-    }else if (isAtTheTop(blogContainer)) {
-        allNavLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
-        allNavLinks[3].classList.add("active-link");
-    }else if (isAtTheTop(contactContainer)) {
-        allNavLinks.forEach(link=>{
-            link.classList.remove("active-link")
-        })
-        allNavLinks[4].classList.add("active-link");
-    }
-}, 100);
+const worksH1 = document.querySelector(".works-h1");
 
-
-// FOR SWIPER
-
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("working");
+    setInterval(() => {
+        if (isAtTheTop(home)) {
+            console.log(1);
+            allNavLinks.forEach(link=>{
+                link.classList.remove("active-link")
+            })
+        }else if (isAtTheTop(workContainer)) {
+            console.log(2);
+            allNavLinks.forEach(link=>{
+                link.classList.remove("active-link")
+            })
+            allNavLinks[0].classList.add("active-link");
+            
+            worksH1.style.top = 0;
+        }else if (isAtTheTop(servicesContainer)) {
+            allNavLinks.forEach(link=>{
+                link.classList.remove("active-link")
+            })
+            allNavLinks[1].classList.add("active-link");
+        }else if (isAtTheTop(pricesContainer)) {
+            allNavLinks.forEach(link=>{
+                link.classList.remove("active-link")
+            })
+            allNavLinks[2].classList.add("active-link");
+        }else if (isAtTheTop(blogContainer)) {
+            allNavLinks.forEach(link=>{
+                link.classList.remove("active-link")
+            })
+            allNavLinks[3].classList.add("active-link");
+        }else if (isAtTheTop(contactContainer)) {
+            allNavLinks.forEach(link=>{
+                link.classList.remove("active-link")
+            })
+            allNavLinks[4].classList.add("active-link");
+        }
+    }, 100);
+})
 
 // SWIPER
 
